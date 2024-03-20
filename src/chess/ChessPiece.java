40245454f5529aaacae0e09b62229f17a4e4d4a0
Leandro Vinícius da Mata Silva <vini.damatta17@gmail.com>
2024-03-20 16:03:12 -0300

    Possible moves of the King

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -15,14 +15,13 @@ public abstract class ChessPiece extends Piece {
 		this.color = color;
 	}
 	
-	protected boolean isThereOpponentPiece(Position position) {
-		ChessPiece p = (ChessPiece)getBoard().piece(position);
-		return p != null && p.getColor() != color;
-	}
-	
 	public Color getColor() {
 		return color;
 	}
 	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 
 }
